6d01cbc46701aebe2c11beff1dbf8bf4cdfdc53d
Anton Lindskog <lindskog96@live.se>
2024-10-21 11:27:25 +0200

    inital commit

---
+++ src/main/java/com/antonspring/ordering_system/entity/Order.java
@@ -0,0 +1,81 @@
+package com.antonspring.ordering_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column (name = "order_name")
+    private String orderName;
+
+    @Column (name = "quantity")
+    private int quantity;
+
+    @Column (name = "price")
+    private double price;
+
+    @Column (name = "status")
+    private String status;
+
+
+
+    public Order(String orderName, int id, int quantity, double price, String status) {
+        this.orderName = orderName;
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+        this.status = status;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
